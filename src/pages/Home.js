11d27c92e06288c070a668a7e073164e090d52ac
Mario Rizki <mariorizki07@gmail.com>
2022-12-12 03:03:13 +0700

    test link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div className="bg-black h-[100vh] p-10 text-center">
       <h1 className="text-[#FFC93C] text-5xl">Reuni Akbar IKA-PMII Komfapsi</h1>
+      <Link to="/invitation/YWVQWy6oHLq8cVhfWO5v">
+        <p className="text-white">Hello</p>
+      </Link>
     </div>
   );
 };
