e69f00b2d8fd91eae221786967d625028e3b2cbc
Mario Rizki <mariorizki07@gmail.com>
2022-12-13 05:51:09 +0700

    overflow x auto for mcomment

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -38,7 +38,7 @@ const Comments = ({
       <Zoom cascade>
         {comments.map((comment) => (
           <div key={comment.timestamp}>
-            <div className="text-start bg-[#0F1744] rounded-md p-3 mt-2">
+            <div className="text-start bg-[#0F1744] rounded-md p-3 mt-2 overflow-x-auto">
               <h1 className="text-xl font-marck font-bold text-amber-500">
                 {comment.owner}
               </h1>
