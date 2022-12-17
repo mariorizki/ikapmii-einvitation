f03ad5233aa184072ed2da95dee01921ab9688d8
Mario Rizki <mariorizki07@gmail.com>
2022-12-17 10:03:38 +0700

    fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,14 @@ const Footer = () => {
   return (
     <>
       <Zoom>
-        <p className="mt-10">
-          Merupakan suatu kehormatan dan kebanggaan bagi kami apabila sahabat/i
-          berkenan hadir. Atas waktu dan kehadirannya kami ucapkan terima kasih.
-        </p>
-        <p>Wassalamualaikum Wr. Wb.</p>
+        <div className="p-7">
+          <p className="mt-10 text-sm">
+            Merupakan suatu kehormatan dan kebanggaan bagi kami apabila
+            sahabat/i berkenan hadir. Atas waktu dan kehadirannya kami ucapkan
+            terima kasih.
+          </p>
+          <p className="text-sm">Wassalamualaikum Wr. Wb.</p>
+        </div>
       </Zoom>
       <div className="bg-[#0F1744] w-full p-2 mt-16">
         <p className="font-extrabold">Powered By Mario Rizki</p>
