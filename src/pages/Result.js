a762dd1f83a84ada647192ea9cc95a6218b601e0
Mario Rizki <mariorizki07@gmail.com>
2022-12-24 14:48:01 +0700

    add details on vote

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -49,6 +49,20 @@ const Result = () => {
       },
     ],
   };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        labels: {
+          // This more specific font property overrides the global property
+          font: {
+            size: 14,
+          },
+        },
+      },
+    },
+  };
   return (
     <>
       {isLoading ? (
@@ -65,7 +79,20 @@ const Result = () => {
           <div className="w-[320px] h-[90%] md:w-[700px]">
             <h1 className="text-lg font-bold">Hasil Vote</h1>
             <h1>Total suara masuk: {total}</h1>
-            <Bar data={chartData} />
+            <Bar data={chartData} options={options} />
+            <h1 className="mt-5">Details:</h1>
+            <div className="mt-3">
+              {candidates.map((candidate) => (
+                <div className="flex justify-between">
+                  <div>
+                    <h1 className="my-1">{candidate.name}</h1>
+                  </div>
+                  <div>
+                    <h1 className="my-1">{candidate.count} Suara</h1>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       )}
--- src/pages/SubmitVote.js
+++ src/pages/SubmitVote.js
@@ -7,12 +7,12 @@ const SubmitVote = () => {
     <div className="bg-[#060E31] text-white h-[100vh] w-full flex flex-col gap-3 justify-center items-center">
       <h1>Terima Kasih!</h1>
       <h1>Voting anda telah diproses</h1>
-      <button
+      {/* <button
         onClick={() => navigate('/result')}
         className="outline-none text-md text-white font-medium py-2 px-3 mt-5 bg-amber-500  rounded-md"
       >
         Lihat hasil
-      </button>
+      </button> */}
     </div>
   );
 };
